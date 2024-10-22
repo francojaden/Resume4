2ca94397d57de86e4c43b8fbbc1ce2e3e2af549d
francojaden <2024161140@ub.edu.bz>
2024-10-22 10:20:31 -0600

    Add files via upload

---
+++ cool.js
@@ -0,0 +1,4 @@
+document.getElementById("fun-button").addEventListener("click", function() {
+    const message = document.getElementById("fun-message");
+    message.classList.toggle("show"); // Toggle the visibility of the message
+});
